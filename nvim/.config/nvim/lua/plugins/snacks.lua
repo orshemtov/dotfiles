6b2f24bdc9d257e1c43bdcc5977416d0364cf1ee
Or Shemtov <oriesh@icloud.com>
2025-10-11 15:53:24 +0300

    showing hidden files on snacks explorer

---
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,18 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      hidden = true,
+      ignored = false,
+      exclude = {},
+
+      sources = {
+        files = {
+          hidden = true,
+          ignored = false,
+          exclude = {},
+        },
+      },
+    },
+  },
+}
