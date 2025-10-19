caed5ed9edfe3e1b22099d5eb29c18efcb14622d
Or Shemtov <oriesh@icloud.com>
2025-10-19 21:56:03 +0300

    sort out snacks, remove copilot chat, configure lualine

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -1,6 +1,39 @@
+-- The text was generated with this online tool:
+-- https://patorjk.com/software/taag/#p=display&f=ANSI+Shadow&t=Type+Something+%0A&x=none&v=4&h=4&w=80&we=false
+
 return {
   "folke/snacks.nvim",
   opts = {
+    dashboard = {
+      preset = {
+        pick = function(cmd, opts)
+          return LazyVim.pick(cmd, opts)()
+        end,
+        header = [[
+        ██████╗ ██████╗ ███████╗
+        ██╔═══██╗██╔══██╗██╔════╝
+        ██║   ██║██████╔╝███████╗
+        ██║   ██║██╔══██╗╚════██║
+        ╚██████╔╝██║  ██║███████║
+        ╚═════╝ ╚═╝  ╚═╝╚══════╝
+ ]],
+        -- stylua: ignore
+        ---@type snacks.dashboard.Item[]
+        keys = {
+          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = "󰺯 ", key = "/", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+          { icon = "",  key = "g", desc = "Git", action = function() Snacks.lazygit( { cwd = LazyVim.root.git() }) end },
+          { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+          { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+          { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
+          { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+          { icon = " ", key = "m", desc = "Mason", action = ":Mason" },
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+        },
+      },
+    },
+
     explorer = {
       auto_open = false,
     },
