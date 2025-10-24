0a413b50b94be7544c037dbf2911bca5243a3557
Or Shemtov <oriesh@icloud.com>
2025-10-25 02:09:22 +0300

    go 8 tab, color scheme, text case, remove copilot

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,12 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "go",
+  callback = function()
+    vim.opt_local.expandtab = false
+    vim.opt_local.tabstop = 8
+    vim.opt_local.shiftwidth = 8
+    vim.opt_local.softtabstop = 8
+  end,
+})
--- nvim/.config/nvim/lua/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -1,4 +1,10 @@
 return {
   "akinsho/bufferline.nvim",
-  enabled = false,
+  enabled = false, -- I keep this off, tabline above is cluttering my workspace
+  optional = true,
+  opts = function(_, opts)
+    if (vim.g.colors_name or ""):find("catppuccin") then
+      opts.highlights = require("catppuccin.special.bufferline").get_theme()
+    end
+  end,
 }
