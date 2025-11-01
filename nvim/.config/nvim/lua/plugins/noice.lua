b202d7d2c1f780f459be3d13bac8a4b6fdb69b7b
Or Shemtov <oriesh@icloud.com>
2025-11-01 23:59:49 +0200

    noice

--- nvim/.config/nvim/lua/plugins/noice.lua
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -1,4 +1,34 @@
 return {
   "folke/noice.nvim",
-  enabled = false,
+  event = "VeryLazy",
+  dependencies = {
+    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+    "MunifTanjim/nui.nvim",
+    -- OPTIONAL:
+    --   `nvim-notify` is only needed, if you want to use the notification view.
+    --   If not available, we use `mini` as the fallback
+    -- "rcarriga/nvim-notify",
+  },
+  opts = {
+    lsp = {
+      hover = { enabled = false },
+      signature = { enabled = false },
+      override = {
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        ["vim.lsp.util.stylize_markdown"] = true,
+      },
+    },
+    presets = {
+      command_palette = true, -- position the cmdline and popupmenu together
+      long_message_to_split = true, -- long messages will be sent to a split
+    },
+    views = {
+      cmdline_popup = {
+        position = {
+          row = "50%",
+          col = "50%",
+        },
+      },
+    },
+  },
 }
