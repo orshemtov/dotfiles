79bdf5877ddaa953f784f4d3bf29f52a3ab692a8
Or Shemtov <oriesh@icloud.com>
2025-11-02 21:02:37 +0200

    add uv plugin

---
+++ nvim/.config/nvim/lua/plugins/uv.lua
@@ -0,0 +1,38 @@
+return {
+  "benomahony/uv.nvim",
+  ft = { "python" },
+  dependencies = {
+    "folke/snacks.nvim",
+  },
+  opts = {
+    -- Auto-activate virtual environments when found
+    auto_activate_venv = true,
+    notify_activate_venv = true,
+
+    -- Auto commands for directory changes
+    auto_commands = true,
+
+    -- Integration with snacks picker
+    picker_integration = true,
+
+    keymaps = {
+      prefix = "<leader>\\",
+      commands = false,
+      run_file = true,
+      run_selection = true,
+      run_function = true,
+      venv = false,
+      init = false,
+      add = true,
+      remove = false,
+      sync = true,
+      sync_all = false,
+    },
+
+    execution = {
+      run_command = "uv run python",
+      notify_output = true,
+      notification_timeout = 10000,
+    },
+  },
+}
